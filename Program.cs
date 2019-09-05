27c3a2a1fcf8721663cde2c348413b756bfcddb4
Sasa23 <Sasa23@SASA>
2019-09-05 14:29:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pause to display the message below of "Hello World!"
+            Console.WriteLine("Press any key to continue....");
+            Console.WriteLine("Hello World! My name is Sasa Subotic, and this is my first project.");
+            Console.ReadKey(true);
+        }
+    }
+}
