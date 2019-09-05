4a55fee8b98910dfa0aa8f92f6bcf4bce0eb9984
Sasa23 <Sasa23@SASA>
2019-09-05 14:32:27 -0400

    Put 4 dots instead of 3 besides the "Press any key..." message. Also need to save/sync into GitHub this way.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             //Pause to display the message below of "Hello World!"
-            Console.WriteLine("Press any key to continue....");
+            Console.WriteLine("Press any key to continue...");
             Console.WriteLine("Hello World! My name is Sasa Subotic, and this is my first project.");
             Console.ReadKey(true);
         }
